e5720f80759c5e2a765482efd0738a8feb8ecb5f
Roman Sidorov <vf@python.ru>
2019-07-18 20:29:10 +0300

    'User_add'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
+from webapp.forms import LoginForm
 from webapp.model import db, News
-
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
@@ -13,9 +13,16 @@ def create_app():
 
     @app.route("/")
     def index():
-        title = 'Прогноз погоды'
+        title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query. order_by(News.published.desc()). all()
         return render_template('index.html', page_title=title, weather_text=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
 
     return app
